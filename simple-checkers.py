1f4cf5eac7a63a23c18be4910d61f7e9afc2cc5b
Abdullah Ibn Arefin <programming.ibn@protonmail.com>
2023-04-21 17:06:28 +0600

    Refractored simple-checkers.py

--- simple-checkers.py
+++ simple-checkers.py
@@ -14,12 +14,38 @@ LIGHT_BROWN = (237, 201, 175)
 DARK_BROWN = (159, 129, 112)
 
 
+# -- Functions --
+
+# As the name suggests this draws the board
+def draw_board(screen, screen_width, square_size, square_color):
+    square_pos_x = square_size
+    square_pos_y = 0
+    above_square_bw = 0  # Checks if the square above is b/w
+
+
+    for _ in range(32):
+        pygame.draw.rect(
+            screen, square_color, (
+                square_pos_x, square_pos_y, square_size, square_size
+                )
+        )
+
+        square_pos_x += square_size * 2
+
+        if square_pos_x > screen_width - square_size:
+            square_pos_y += square_size
+
+            if above_square_bw == 0:
+                square_pos_x = 0
+                above_square_bw = 1
+            else:
+                square_pos_x = square_size
+                above_square_bw = 0
+
+
+
 # -- Main loop      --
 while True:
-    square_pos_x = SQUARE_SIZE
-    square_pos_y = 0
-    above_square_occ = 0  # Checks if the square above is occupied
-
     # Checks for events like close & minimize
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -27,22 +53,6 @@ while True:
 
     SCREEN.fill(LIGHT_BROWN)
 
-    # Draws the board
-    for i in range(32):
-        pygame.draw.rect(
-            SCREEN, DARK_BROWN, (square_pos_x, square_pos_y, SQUARE_SIZE, SQUARE_SIZE)
-        )
+    draw_board(SCREEN, SCREEN_WIDTH, SQUARE_SIZE, DARK_BROWN)
 
-        square_pos_x += SQUARE_SIZE * 2
-
-        if square_pos_x > SCREEN_WIDTH - SQUARE_SIZE:
-            square_pos_y += SQUARE_SIZE
-
-            if above_square_occ == 0:
-                square_pos_x = 0
-                above_square_occ = 1
-            else:
-                square_pos_x = SQUARE_SIZE
-                above_square_occ = 0
-
-    pygame.display.update()
+    pygame.display.flip()
